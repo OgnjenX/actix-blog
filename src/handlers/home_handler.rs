28dfc4fcaf697a9c69986d61095fc91bc5e9511e
ocadovski <ognjen.cadovski@robbit.rs>
2024-12-12 14:07:06 +0100

    Improve error handling in home_handler.rs by introducing FrontmatterError enum.

--- src/handlers/home_handler.rs
+++ src/handlers/home_handler.rs
@@ -1,7 +1,8 @@
 use actix_web::{get, web, HttpResponse, Responder};
 use ignore::WalkBuilder;
 use serde::{Deserialize, Serialize};
-use std::{fs, io::Error};
+use std::fs;
+use thiserror::Error;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Frontmatter {
@@ -15,50 +16,48 @@ pub struct Frontmatter {
     order: u32,
 }
 
-fn find_all_frontmatters() -> Result<Vec<Frontmatter>, std::io::Error> {
+#[derive(Error, Debug)]
+pub enum FrontmatterError {
+    #[error("IO error: {0}")]
+    IoError(#[from] std::io::Error),
+    #[error("TOML parsing error: {0}")]
+    TomlError(#[from] toml::de::Error),
+    #[error("Other error: {0}")]
+    OtherError(String),
+}
+
+fn find_all_frontmatters() -> Result<Vec<Frontmatter>, FrontmatterError> {
     let mut t = ignore::types::TypesBuilder::new();
     t.add_defaults();
-    let toml = match t.select("toml").build() {
-        Ok(t) => t,
-        Err(e) => {
-            println!("{:}", e);
-            return Err(Error::new(
-                std::io::ErrorKind::Other,
-                "could not build toml file type matcher",
-            ));
-        }
-    };
-
+    let toml = t
+        .select("toml")
+        .build()
+        .map_err(|e| FrontmatterError::OtherError(e.to_string()))?;
     let file_walker = WalkBuilder::new("./posts").types(toml).build();
-
     let mut frontmatters = Vec::new();
     for frontmatter in file_walker {
         match frontmatter {
             Ok(fm) => {
                 if fm.path().is_file() {
                     let fm_content = fs::read_to_string(fm.path())?;
-                    let frontmatter: Frontmatter = toml::from_str(&fm_content).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
-
+                    let frontmatter: Frontmatter = toml::from_str(&fm_content)?;
                     frontmatters.push(frontmatter);
                 }
             }
             Err(e) => {
-                println!("{:}", e); // we're just going to print the error for now
-                return Err(Error::new(
-                    std::io::ErrorKind::NotFound,
-                    "could not locate frontmatter",
+                println!("{:}", e);
+                return Err(FrontmatterError::OtherError(
+                    "could not locate frontmatter".to_string(),
                 ));
             }
         }
     }
-
     Ok(frontmatters)
 }
 
 #[get("/")]
 pub async fn index(templates: web::Data<tera::Tera>) -> impl Responder {
     let mut context = tera::Context::new();
-
     let mut frontmatters = match find_all_frontmatters() {
         Ok(fm) => fm,
         Err(e) => {
@@ -69,9 +68,7 @@ pub async fn index(templates: web::Data<tera::Tera>) -> impl Responder {
         }
     };
     frontmatters.sort_by(|a, b| b.order.cmp(&a.order));
-
     context.insert("posts", &frontmatters);
-
     match templates.render("home.html", &context) {
         Ok(s) => HttpResponse::Ok().content_type("text/html").body(s),
         Err(e) => {
